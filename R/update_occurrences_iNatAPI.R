974dcedb9f74dc1ef18f969f14b330680cb0f3e4
Shelly Lachish <shellstar@gmail.com>
2024-12-18 11:16:51 +1000

    adds spatial intersect with toohey shapefile

--- R/update_occurrences_iNatAPI.R
+++ R/update_occurrences_iNatAPI.R
@@ -32,20 +32,21 @@ message("\nReading in base occurrences ...")
 base_occs <- readr::read_rds("./output_data/toohey_species_occurrences.rds")
 
 
-# Define date range: last 10 days
+# Define date range: from max Date in base data to today
 base_date <- as.Date(max(base_occs$eventDate), format = "%Y-%m-%d")
+max_date <- Sys.Date()
 
-# Define bounding box -----
-swlat <- -27.561382
-swlng <- 153.030074
-nelat <- -27.531057
-nelng <- 153.082805
+# Define bounding box - iNat API doesn't accept shapefily to geo limit -----
+nelat <- -27.5312
+nelng <- 153.082
+swlat <- -27.5625
+swlng <- 153.0302
 
 # Build the API query string for Taxa of interest ----
 taxa_ids <- c(3, 40151, 26036)  # Birds, Mammals, Reptiles
 query_str <- construct_api_query(taxa_ids,
                                  min_date = base_date,
-                                 max_date = Sys.Date())
+                                 max_date = max_date)
 
 # Base URL for observations -----
 base_url <- "https://api.inaturalist.org/v1/observations"
@@ -53,7 +54,7 @@ base_url <- "https://api.inaturalist.org/v1/observations"
 # Get the latest observations  -----
 message("\nDownloading occurrences ...")
 
-new_occurrences <- data.frame()
+new_occs <- data.frame()
 page_num <- 1
 
 while (TRUE) {
@@ -71,19 +72,25 @@ while (TRUE) {
   existing_fields <- fields[fields %in% names(parsed$results)]
 
   observations_df <- parsed$results[, existing_fields, drop = FALSE]
-  new_occurrences <- dplyr::bind_rows(new_occurrences, observations_df)
+  new_occs <- dplyr::bind_rows(new_occs, observations_df)
 
   page_num <- page_num + 1
 }
 
 # Tidy the data and get cladistics  -----
-new_occurrences_formatted <- tidy_api_newoccs(new_occurrences)
+new_occs_tidy <- tidy_api_newoccs(new_occs)
 
+
+# Spatially filter the data to retain only occurrences in our shapefile ----
+# Read in the Toohey Forest Boundary shapefile to limit occurrences
+toohey_outline <- sf::st_read("./spatial_data/toohey_forest_boundary.shp")
+
+new_occs_toohey <- do_spatial_intersect(new_occs_tidy, toohey_outline)
 
 # Add cladistics ----
 message("Adding cladistics ...")
-api_clad_data <- galah::search_taxa(new_occurrences_formatted$taxon.name) |> distinct()
-occ_updates_cladistics <- add_cladistics(occ_data = new_occurrences_formatted,
+api_clad_data <- galah::search_taxa(new_occs_toohey$taxon.name) |> distinct()
+occ_updates_cladistics <- add_cladistics(occ_data = new_occs_toohey,
                                          clad_data = api_clad_data,
                                          type = "iNat")
 
@@ -100,14 +107,14 @@ if (any(test_results == "expectation_failure")) {
   } else {
     message("\nAll tests passed. Proceeding with further analysis.")
 
-    new_occs <- occ_updates_cladistics |>
+    new_occs_to_add <- occ_updates_cladistics |>
       dplyr::anti_join(base_occs |>
                          select(latitude, longitude, eventDate, eventTime, species))
 
-    message(paste0("\n\nNumber of new occurrences added: ", dim(new_occs)[1]))
+    message(paste0("\n\nNumber of new occurrences added: ", dim(new_occs_to_add)[1]))
 
     # Row bind, remove duplicates and save (overwrite)
-    updated_occ_data <- dplyr::bind_rows(base_occs, new_occs)
+    updated_occ_data <- dplyr::bind_rows(base_occs, new_occs_to_add)
 
     message(paste0("\n\nTotal number of occurrences in data: ", dim(updated_occ_data)[1]))
 
