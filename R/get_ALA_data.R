897e1fc5ab9d57888c9925b7b151f3d6fcaec8e1
Shelly Lachish <shelly.lachish@health.qld.gov.au>
2025-01-23 09:48:57 +1000

    adds amphibia, 10 years and moves stuff to steering

--- R/get_ALA_data.R
+++ R/get_ALA_data.R
@@ -1,39 +1,28 @@
-library(galah)
-library(dplyr)
-library(readr)
-library(sf)
+#===============================================================================
+# Script Name: get_ALA_data.R
+# Description: Downloads and processes species occurrence data from the Atlas of
+#              Living Australia (ALA) for Toohey Forest, Brisbane. Uses the galah
+#              package to interface with ALA and spatial data to define the area
+#              of interest.
+#
+# Dependencies: galah, dplyr, readr, sf
+#
+# Outputs: Species occurrence data for Toohey Forest area
+#===============================================================================
 
-
-# Configure ALA
+# Configure ALA ----
 galah::galah_config(atlas = "Australia",
              email = "shelly.lachish@csiro.au",
              download_reason_id = "citizen science"
              )
 
 
-# Source functions ----
-source("./R/functions.R")
-
-
-#> Define the Toohey bounding box ----
-# nw = -27.531057, 153.035337
-# ne = -27.537956, 153.082805
-# se = -27.561382, 153.078571
-# sw = -27.555471, 153.030074
-
-# b_box <- sf::st_bbox(c(xmin = 153.0302, xmax = 153.082,
-#                        ymin = -27.5312, ymax = -27.53855),
-#                      crs = sf::st_crs("WGS84"))
-
-
-# Read in the Toohey Forest Boundary shapefile to limit occurrences
-toohey_outline <- sf::st_read("./spatial_data/toohey_forest_boundary.shp")
-
-
 ## Functions to extract data from the ALA Interface [galah package] ----
 
-# Get occurrences of all reptiles, birds and mammals for past 5 years
-toohey_occurrences <- get_occurrences(year = 2024 - 5, month = 1, geo_limit = toohey_outline)
+# Get occurrences of all reptiles, birds and mammals, amphibians for past 5 years
+toohey_occurrences <- get_occurrences(year = this_year - years_past,
+                                      month = 1,
+                                      geo_limit = toohey_outline)
 toohey_occurrences_formatted <- tidy_ala_data(toohey_occurrences)
 
 
@@ -43,7 +32,9 @@ ala_clad_data <- galah::search_taxa(unique(toohey_occurrences_formatted$scientif
 # Get cladistics - Join cladistics to this and filter to remove duplicates
 occ_cladistics <- add_cladistics(occ_data = toohey_occurrences_formatted,
                                  clad_data = ala_clad_data,
-                                 type = "ALA")
+                                 type = "ALA") |>
+  # There was a single row with NA for vernacular name
+  tidyr::drop_na()
 
 
 # Save this dataset as the base data - (the github action will just run the update script in the future)
