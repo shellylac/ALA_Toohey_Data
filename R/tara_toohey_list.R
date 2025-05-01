731bc5efbcc82e58c57c42d8a2c05ef513c665d6
Shelly Lachish <shellstar@gmail.com>
2025-05-01 17:01:30 +1000

    script to check for tara toohey occs

---
+++ R/tara_toohey_list.R
@@ -0,0 +1,36 @@
+# Function to create a subset for species in Tara Toohey part of reserve
+create_select_spp_df <- function(data, spp_list) {
+  select_data <- data |>
+    dplyr::filter(eventDate >= Sys.Date()-2,
+                  vernacular_name %in% spp_list
+    ) |>
+    # apply the broad spatial filter for Tara Toohey
+    filter(latitude <= -27.530755,
+           latitude >= -27.541794,
+           longitude >= 153.033571,
+           longitude <= 153.039194
+    ) |>
+    # Keep selected cols
+    select(vernacular_name, eventDate, eventTime, google_maps_url)
+
+  return(select_data)
+}
+
+# Read in the base occs dataset
+base_occ_dataset <- readRDS("./output_data/toohey_species_occurrences.rds")
+
+# Define the species of interest
+my_species = c("Koala",
+             "Squirrel Glider",
+             "Feathertail Glider",
+             "Short-beaked Echidna")
+
+tara_toohey_df <- create_select_spp_df(data = base_occ_dataset,
+                                       spp_list = my_species)
+
+
+# ── only print when there's data ─────────────────────────────────────────────
+if (nrow(tara_toohey_df) > 0) {
+  print(tara_toohey_df)
+}
+
